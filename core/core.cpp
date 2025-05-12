3918d181b9d753acb023ceae6db909e633493e40
Linkan333 <linunybe@gmail.com>
2025-05-12 21:36:03 +0200

    First Commit

---
+++ core/core.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+#include <unordered_map>
+#include <iomanip>
+#include <math.h>
+#include <stdio.h>
+
+void atomicMap(std::unordered_map<std::string, double>& atomicList) {
+    atomicList = {
+        {"Electron", 9.10932e-31},
+        {"Proton", 1.672622e-27},
+        {"Neutron", 1.674927e-27},
+        {"Elementar", 1.602176634e-19},
+        {"Nucleon", 1.6726e-27},
+        {"u", 1.660539e-27} // Atomic mass
+    };
+}
+
+void radioactiveDecay() {
+    // Radioactive decay over a certain time span
+    // Formula: (N = N0e-λt)
+    double N_0; // Radioactive cores ex: uran-235 (92 protons and 143 neutrons wich we can use the atomics neuclon algorithm for)
+    double t = 1.0e9; // Over t (time) this example has 1*10^9 wich is 1 billion years
+    double T_half = 7.038e8; // Half-life of uranium-235 in years (~703.8 million years)
+    double lambda = log(2.0) / T_half;
+    double N;
+
+    // Calculating remaining nucleons using N = N_0 * e^(-lambda * t)
+    N = N_0 * exp(-lambda * t);
+    
+    std::cout << "Initial nuclei (N_0): \n" << N_0 << std::endl;
+    std::cout << "Time elapsed (t): " << t << " years\n" << std::endl;
+    std::cout << "Half-life (T_1/2): " << T_half  << " years\n" << std::endl;
+    std::cout << "Decay constant (lambda): " << lambda << " per year\n" << std::endl;
+    std::cout << "Remainign nucleon (N)\n" << N << std::endl;
+}
+
+int main() {
+    std::unordered_map<std::string, double> atomicList;
+    atomicMap(atomicList);
+
+    // Print all items with formatted output
+    std::cout << std::scientific << std::setprecision(3);
+    for (const auto& pair : atomicList) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+
+
+    // Demo for atomics neutron count. 
+    int N;
+    int Z;
+    
+    std::cout << "Enter the amount of Neutrons: " << std::endl;
+    std::cin >> N;
+    std::cout << "Enter the amount of Protons: " << std::endl;
+    std::cin >> Z;
+
+    // For helium example:
+    int A = N + Z;
+    std::cout << "\nCount of Nucleons: " << A << std::endl;
+
+
+    radioactiveDecay();
+
+    return 0;
+}
